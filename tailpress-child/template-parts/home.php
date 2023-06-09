43d7be014c80f14e3b7d90b88ad69e4cb987b285
nathan-evol <nathan@agence-evol.fr>
2023-06-09 11:58:43 +0200

    adding tailpress child

---
+++ tailpress-child/template-parts/home.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Homepage */
+
+get_header();
+?>
+
+<div class="container">
+    <div class="row">
+        <p class="test">Template de base</p>
+    </div>
+</div>
+
+<?php get_footer() ?>
